9dee47a08f11d59a46d6ad43855e7f1f3100315f
Abinaya47 <144511494+Abinaya47@users.noreply.github.com>
2025-04-26 15:33:27 +0530

    Delete auth.py

--- auth.py
+++
@@ -1,7 +0,0 @@
-def authenticate(username, password):
-    """
-    Check if the provided username and password match the stored credentials.
-    """
-    stored_username = 'brainwave'
-    stored_password = '2005'
-    return username == stored_username and password == stored_password
